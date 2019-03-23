5394e7810bb6ed09b8516918a3dbfbd87d1b1dd2
AmitFernandez <amit.fernandez@hotmail.com>
2019-03-23 13:15:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,6 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-if __name__ == '__main__':
-    app.run()
 
 class Chat:
     def __init__(self, userName, message, timecreated):
